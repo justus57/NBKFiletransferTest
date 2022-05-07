d5b6abaecac794c3cb20e1e605ba7218b8eabb59
JUSTUS57 <justuskasyoki57@gmail.com>
2022-05-07 13:28:39 +0300

    COMMIT

--- Program.cs
+++ Program.cs
@@ -41,39 +41,39 @@ namespace NBKFiletransferTest
             string Response = null;
             try
             {
-                try
-                {
-                    ///perform encryption using cmd  usin g the encyrptor
-                    Process cmd = new Process();
-                    cmd.StartInfo.FileName = "cmd.exe";
-                    cmd.StartInfo.RedirectStandardInput = true;
-                    cmd.StartInfo.UseShellExecute = false;
-                    cmd.Start();
-                    using (StreamWriter sw = cmd.StandardInput)
-                    {
-                        Utility.WriteLog("Encryptioning......");
-                        if (sw.BaseStream.CanWrite)
-                        {
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine("cd ..");
-                            sw.WriteLine(@"cd C:\Users\Admin2\Downloads\EncyptionTool\EncyptionTool");
-                            // sw.WriteLine("java -jar KeyGeneratorForEncryption.jar");
-                            sw.WriteLine("java -jar FileEncryptor.jar");
-                            Utility.WriteLog("Encryption successful!");
-                            Response = "Encryption successful!";
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Utility.WriteLog(ex.Message);
-                    Response = "Encryption Unsuccessful!";
-                }
+                //try
+                //{
+                //    ///perform encryption using cmd  usin g the encyrptor
+                //    Process cmd = new Process();
+                //    cmd.StartInfo.FileName = "cmd.exe";
+                //    cmd.StartInfo.RedirectStandardInput = true;
+                //    cmd.StartInfo.UseShellExecute = false;
+                //    cmd.Start();
+                //    using (StreamWriter sw = cmd.StandardInput)
+                //    {
+                //        Utility.WriteLog("Encryptioning......");
+                //        if (sw.BaseStream.CanWrite)
+                //        {
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine("cd ..");
+                //            sw.WriteLine(@"cd C:\Users\Admin2\Downloads\EncyptionTool\EncyptionTool");
+                //            // sw.WriteLine("java -jar KeyGeneratorForEncryption.jar");
+                //            sw.WriteLine("java -jar FileEncryptor.jar");
+                //            Utility.WriteLog("Encryption successful!");
+                //            Response = "Encryption successful!";
+                //        }
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    Utility.WriteLog(ex.Message);
+                //    Response = "Encryption Unsuccessful!";
+                //}
                 SendPaymentFile(host, username, password, port);
             }
             catch (Exception es)
@@ -107,7 +107,7 @@ namespace NBKFiletransferTest
                     {
                         Console.WriteLine("I'm connected to the client");
 
-                        string[] filePaths = Directory.GetFiles(localFilePath, "*.xlsx");
+                        string[] filePaths = Directory.GetFiles(inputfileEncryptor, "*.xlsx");
                         
                         List<string> lst = filePaths.ToList();
 
